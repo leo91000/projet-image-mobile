db499f99c50dae15c20b48f11db26d31f899fbbb
= <mail.leo.coletta@gmail.com>
2020-01-21 14:04:59 +0100

    Docker file

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views import ImageView, ImageIdView
 
 urlpatterns = [
-    path('img_searches/', ImageView.as_view()),
+    path('img_searches', ImageView.as_view()),
     path('img_searches/<int:img_id>/', ImageIdView.as_view())
 ];
